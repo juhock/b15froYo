f5648369d32cfbd1c577c247fceab132ca88833c
Julia Arfinengo <juliaarfinengo@Julias-MacBook-Pro-2.local>
2023-09-21 14:50:22 -0500

    start of repo

---
+++ froYo.js
@@ -0,0 +1,12 @@
+const froYo = {
+    va: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    soupAndSalad: 16
+};
+
+
+console.log(Object.keys(dinner));
+
+console.log(Objects.values(dinner))
